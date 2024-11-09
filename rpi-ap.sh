46f8db0f0e802ddf047f92c07c2f6c92c0115266
BwithE <144924113+BwithE@users.noreply.github.com>
2024-11-08 23:37:09 -0500

    Update rpi-ap.sh

--- rpi-ap.sh
+++ rpi-ap.sh
@@ -10,7 +10,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-clear
+
 echo "
 
     ____        _       ___                            ____        _       __
@@ -24,12 +24,12 @@ echo "
 
 echo "+-----------------------------------------------+"
 read -p "| What would you like the SSID to be?: " ssid
-read -p "| What would you like the passphrase to be?: " pass
+read -s -p "| What would you like the passphrase to be?: " pass
 read -p "| Would you like to have a Web based portal? (y/n): " rpiapcp
 read -p "| What channel would you like your network to run on? (ex: 1,6,11): " channel
 read -p "| What network card would you like to use? (or press Enter for default: 'wlan0'): " wificard
 read -p "| How many user's would you like to be able to join this network? (2-20): " allowed_ips
-read -p "| Will this AP be used with a VPN? (y/n): " vpn
+read -p "| Will this AP be used with a VPN? (Wireguard / OpenVPN) (y/n): " vpn
 echo "+-----------------------------------------------+"
 
 # use default value "wlan0" if the user presses Enter without typing anything
@@ -42,16 +42,26 @@ if [ -z "$vpn" ]; then
 fi
 # Check if any variable is not answered, then exit the script
 if [ -z "$ssid" ] || [ -z "$pass" ] || [ -z "$channel" ] || [ -z "$allowed_ips" ]; then
-  echo "+--------------------------------------------------------+"
-  echo "Error: Please provide values for all variables. Exiting..."
-  echo "+--------------------------------------------------------+"
+  echo "+----------------------------------------------------------+"
+  echo "|Error: Please provide values for all variables. Exiting...|"
+  echo "+----------------------------------------------------------+"
   exit 1
 fi
 
 # sets VPN settings based off users vpn cert location
 if [ "$vpn" = "yes" ]; then
   echo "+--------------------------------------------------------+"
-  read -p "Please specify full path for your VPN conf file. (ex: /home/user/user.ovpn): " vpnconf
+  read -p "| Please choose one of the following: 
+| (1) Wireguard 
+| (2) OpenVPN 
+: " vpnchoice
+  echo ""
+  if [ "$vpnchoice" = 1 ]
+  then
+     vpnconf="/etc/wireguard/wg0.conf"
+  elif [ "$vpnchoice" = 2 ]
+     read -p "| Please specify full path for your VPN conf file. (ex: /home/user/user.ovpn): " vpnconf
+  fi
 fi
 
 # Based off user input, the channel specifies the mode
@@ -68,41 +78,58 @@ fi
 
 clear
 echo "+--------------------------------------------------------+"
-echo "This script is about to apply updates and install the necessary applications to make this machine an access point."
+echo "| This script is about to apply updates and install the 
+| necessary applications to make this machine an access point."
 echo "+--------------------------------------------------------+"
 echo "| SSID: $ssid"
 echo "| Password: $pass"
 echo "| Wireless card: $wificard"
 echo "| Mode and Channel: $mode $channel"
 if [ "$vpn" = "yes" ]; then
+  echo "| VPN: $vpnchoice"
   echo "| VPN conf location: $vpnconf"
 fi
-echo "+--------------------------------------------------------+"
+echo "+------------------------------------------------------------------------+"
 echo " "
-echo "| To modify Access Point settings, check the '/etc/hostapd/hostapd.conf'"
+echo "| To modify Access Point settings, check the '/etc/hostapd/hostapd.conf' |"
 echo " "
-echo "+--------------------------------------------------------+"
+echo "+------------------------------------------------------------------------+"
 read -n 1 -r -s -p $'Press enter to continue if the values above are correct. Otherwise "Ctrl + c" to reenter...\n'
-clear
 
+
+echo "+------------------------------------------------------------------------+"
+echo "| APPLYING UPDATES AND INSTALLING NECESSARY APPLICATIONS                 |"
+echo "+------------------------------------------------------------------------+"
+sleep 1
 # Applies update then install required software for the application
 apt-get update -y
 apt-get install hostapd dnsmasq nmap arp-scan pip -y
 pip install flask 
 pip3 install flask --break-system-packages
 DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
-clear
 
-#  installs openvpn
+
+#  installs and updates depending on choice
 if [ "$vpn" = "yes" ]; then
 echo "+--------------------------------------------------------+"
-echo "| Installing OPENVPN & WIREGUARD"
-  apt install openvpn -y
-  apt install wireguard -y
+echo "| Installing and updating VPN                            |"
+echo "+--------------------------------------------------------+"
+   if [ "$vpnchoice" = 1 ]
+   then   
+      apt install wireguard -y && apt update wireguard -y
+      ip route add default dev wg0
+      echo "@reboot wg-quick up wg0" > /etc/cron.d/cronjob
+   elif [ "$vpnchoice" = 2 ]
+   then
+     apt install openvpn -y && apt update openvpn -y
+     ip route add default dev tun0
+     echo "@reboot openvpn $vpnconf" > /etc/cron.d/cronjob
+   fi
+  
 fi
 
 # writes settings to /etc/dhcpcd.conf
-clear
+
 tee -a /etc/dhcpcd.conf << EOF
 interface $wificard
 static ip_address=10.10.10.1/24
@@ -110,16 +137,24 @@ nohook wpa_supplicant
 EOF
 
 # writes routing settings for wireless to eth0
-clear
+
 tee -a /etc/sysctl.d/routed-ap.conf << EOF
 net.ipv4.ip_forward=1
 EOF
 
+# configures iptables to allow NAT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-netfilter-persistent save
+mkdir /etc/iptables
+iptables-save > /etc/iptables/rpiap-tables
+
+#iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#sh -c "iptables-save > /etc/iptables.ipv4.nat"
+
+# enable IP forwarding on boot
+sed -i '/^exit 0/ i iptables-restore < /etc/iptables/rpiap-tables' /etc/rc.local
+echo "@reboot /sbin/iptables-restore < /etc/iptables/rpiap-tables" | sudo tee -a /var/spool/cron/crontabs/root > /dev/null
 
 # writes settings to /etc/network/interfaces
-clear
 tee -a /etc/network/interfaces << EOF
 auto lo
 iface lo inet loopback
@@ -136,7 +171,7 @@ netmask 255.255.255.0
 EOF
 
 # writes settings to /etc/dnsmasq.conf
-clear
+
 mv /etc/dnsmasq.conf /etc/dnsmasq.conf.old
 tee -a /etc/dnsmasq.conf << EOF
 interface=$wificard
@@ -146,7 +181,7 @@ address=/rt.wlan/10.10.10.1
 EOF
 
 # Writes configs to "/etc/hostapd/hostapd.conf
-clear
+
 tee -a /etc/hostapd/hostapd.conf << EOF
 interface=$wificard
 ssid=$ssid
@@ -173,17 +208,10 @@ sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 sysctl -p
 
-# configures iptables to allow NAT
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sh -c "iptables-save > /etc/iptables.ipv4.nat"
 
-# enable IP forwarding on boot
-sed -i '/^exit 0/ i iptables-restore < /etc/iptables.ipv4.nat' /etc/rc.local
 ##########################################
-
-
 # Starts required services and then reboots the machine
-clear
+
 systemctl start hostapd
 systemctl unmask hostapd.service
 systemctl enable hostapd
@@ -191,17 +219,6 @@ systemctl start dnsmasq
 systemctl enable dnsmasq
 systemctl enable ssh
 
-#  sets VPN settings for tun0
-if [ "$vpn" = "yes" ]; then
-echo "+--------------------------------------------------------+"
-echo "| Configuring VPN"
-   openvpn $vpnconf &
-   ip route add default dev tun0
-   sysctl -w net.ipv4.ip_forward=1
-   iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
-   netfilter-persistent save
-   # echo "@reboot openvpn $vpnconf" > /etc/cron.d/cronjob
-fi
 
 # Creating python flask service based off the user who runs this script
 # that user will have permissions to modify settings through the webpage
@@ -209,10 +226,10 @@ fi
 if [ -z "$rpiapcp" ]; then
   rpiapcp="no"
 elif [ "$rpiapcp" = "yes" ]; then
-   clear
+   
    echo "+--------------------------------------------------------+"
    echo "| Creating WebApp for RpiAP"
-   clear
+   
 
    rpiap=$(find / -name "rpiap.py" 2>/dev/null)
    rpidir=$(find / -name "rpiap.py" -exec dirname {} \; 2>/dev/null)
@@ -250,20 +267,28 @@ echo $commos > "$SERVICE_FILE"
 
    echo "| Service file created at: $SERVICE_FILE"
 
-   clear
+   
    # After everythings done running, the PI will reboot
+   echo ""
+   echo "+-----------------------------------------------------------------------------------+"
+   echo ""
    echo "| Once you're connected to '$ssid', Please open a Web-Browser and go to '10.10.10.1'"
+   echo ""
    read -n 1 -r -s -p $'Press enter to reboot.\n'
+   echo "+-----------------------------------------------------------------------------------+"
    reboot
 
    else
-   echo "+--------------------------------------------------------+"
+   echo "+-----------------------------------------------------------------------------------+"
    echo "| INVALID INPUT for WebApp"
    fi
 
 
 
-clear
+
 # After everythings done running, the PI will reboot
+echo "+-----------------------------------------------------------------------------------+"
+echo ""
 read -n 1 -r -s -p $'Press enter to reboot.\n'
+echo ""
 reboot
